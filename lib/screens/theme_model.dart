f591e87af2040d0c6c95e9eb44a27186a5eb069f
Nathan Larsen <nlarsen@nathan2055.com>
2025-10-25 19:07:29 -0400

    Finish implementing ThemeModel

--- lib/screens/theme_model.dart
+++ lib/screens/theme_model.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
 class ThemeModel extends ChangeNotifier {
-  // Theme control variables
-  ThemeMode _themeMode = ThemeMode.light;
-  bool darkMode = false;
+  /*
+  final ThemeMode _lightMode = ThemeMode.light;
+  final ThemeMode _darkMode = ThemeMode.dark;
+  final Icon _lightIcon = const Icon(Icons.sunny);
+  final Icon _darkIcon = const Icon(Icons.mode_night);
+  */
 
+  ThemeMode _themeMode = ThemeMode.light;
+
+  ThemeMode get themeMode => _themeMode;
+  Icon get themeIcon => (_themeMode == ThemeMode.dark)
+      ? const Icon(Icons.sunny)
+      : const Icon(Icons.mode_night);
+
+  //set _themeMode(ThemeMode newMode) => newMode;
+
+  //Icon get _themeIcon => lightIcon;
+  //set _themeIcon(Icon newIcon) => newIcon;
+
+  /*
   ThemeMode getThemeMode() {
     return _themeMode;
   }
@@ -25,6 +41,16 @@ class ThemeModel extends ChangeNotifier {
     } else if (_themeMode == ThemeMode.dark) {
       _themeMode = ThemeMode.light;
       darkMode = false;
+    }
+    notifyListeners();
+  }
+  */
+
+  void toggleThemeMode() {
+    if (_themeMode == ThemeMode.light) {
+      _themeMode = ThemeMode.dark;
+    } else if (_themeMode == ThemeMode.dark) {
+      _themeMode = ThemeMode.light;
     }
     notifyListeners();
   }
