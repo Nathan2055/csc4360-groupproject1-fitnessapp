d765284368d8d121acd9ac691b8582f7c3ca9f87
Nathan Larsen <nlarsen@nathan2055.com>
2025-10-25 18:39:29 -0400

    Further work on converting dark mode to use a ChangeNotifier

--- lib/screens/appbar.dart
+++ lib/screens/appbar.dart
@@ -5,6 +5,30 @@ class ThemeModel extends ChangeNotifier {
   // Theme control variables
   ThemeMode _themeMode = ThemeMode.light;
   bool darkMode = false;
+
+  ThemeMode getThemeMode() {
+    return _themeMode;
+  }
+
+  Icon getThemeIcon() {
+    if (_themeMode == ThemeMode.light) {
+      return Icon(Icons.sunny);
+    } else if (_themeMode == ThemeMode.dark) {
+      return Icon(Icons.mode_night);
+    }
+    return Icon(Icons.sunny);
+  }
+
+  void toggleThemeMode() {
+    if (_themeMode == ThemeMode.light) {
+      _themeMode = ThemeMode.dark;
+      darkMode = true;
+    } else if (_themeMode == ThemeMode.dark) {
+      _themeMode = ThemeMode.light;
+      darkMode = false;
+    }
+    notifyListeners();
+  }
 }
 
 // Constructor
