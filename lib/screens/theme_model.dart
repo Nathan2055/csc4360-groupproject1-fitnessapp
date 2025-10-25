4b54ad85b870bc9973f8bf3fee5043d954508149
Nathan Larsen <nlarsen@nathan2055.com>
2025-10-25 19:34:10 -0400

    Clean up old code from ThemeModel implementation

--- lib/screens/theme_model.dart
+++ lib/screens/theme_model.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ThemeModel extends ChangeNotifier {
-  /*
-  final ThemeMode _lightMode = ThemeMode.light;
-  final ThemeMode _darkMode = ThemeMode.dark;
-  final Icon _lightIcon = const Icon(Icons.sunny);
-  final Icon _darkIcon = const Icon(Icons.mode_night);
-  */
-
   ThemeMode _themeMode = ThemeMode.light;
 
   ThemeMode get themeMode => _themeMode;
@@ -16,37 +9,6 @@ class ThemeModel extends ChangeNotifier {
       : const Icon(Icons.mode_night);
   String get themeText =>
       (_themeMode == ThemeMode.dark) ? 'Light Mode' : 'Dark Mode';
-
-  //set _themeMode(ThemeMode newMode) => newMode;
-
-  //Icon get _themeIcon => lightIcon;
-  //set _themeIcon(Icon newIcon) => newIcon;
-
-  /*
-  ThemeMode getThemeMode() {
-    return _themeMode;
-  }
-
-  Icon getThemeIcon() {
-    if (_themeMode == ThemeMode.light) {
-      return Icon(Icons.sunny);
-    } else if (_themeMode == ThemeMode.dark) {
-      return Icon(Icons.mode_night);
-    }
-    return Icon(Icons.sunny);
-  }
-
-  void toggleThemeMode() {
-    if (_themeMode == ThemeMode.light) {
-      _themeMode = ThemeMode.dark;
-      darkMode = true;
-    } else if (_themeMode == ThemeMode.dark) {
-      _themeMode = ThemeMode.light;
-      darkMode = false;
-    }
-    notifyListeners();
-  }
-  */
 
   void toggleThemeMode() {
     if (_themeMode == ThemeMode.light) {
