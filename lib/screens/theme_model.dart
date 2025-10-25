383a3770a8827d38e7e3b367db5b8526a6ed388e
Nathan Larsen <nlarsen@nathan2055.com>
2025-10-25 18:51:11 -0400

    Continue working on ThemeModel

--- lib/screens/theme_model.dart
+++ lib/screens/theme_model.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class ThemeModel extends ChangeNotifier {
   // Theme control variables
@@ -28,45 +27,5 @@ class ThemeModel extends ChangeNotifier {
       darkMode = false;
     }
     notifyListeners();
-  }
-}
-
-// Constructor
-class AppBar extends StatefulWidget {
-  const AppBar({super.key});
-
-  @override
-  State<AppBar> createState() => _AppBarState();
-}
-
-class _AppBarState extends State<AppBar> {
-  // Title variable
-  Text _titleBar = Text('Recipe Book');
-
-  // Theme control variables
-  ThemeMode _themeMode = ThemeMode.light;
-  bool darkMode = false;
-
-  // Theme control functions; needed since we do the call from another file
-  void _updateThemeMode(ThemeMode mode) {
-    setState(() {
-      _themeMode = mode;
-    });
-  }
-
-  bool _getThemeMode() {
-    return darkMode;
-  }
-
-  void _updateTitleBar(String newtitle) {
-    setState(() {
-      _titleBar = Text(newtitle);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
   }
 }
